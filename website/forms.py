976ee533256258a49baf79f29a4a9618a71812f3
Happy Felix Chukwuma <happychukwuma@gmail>
2024-01-17 00:29:41 +0100

    edit

--- website/forms.py
+++ website/forms.py
@@ -12,14 +12,3 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = '__all__'
 
-"""SHIFT = (
-    ("1", "Morning"),
-    ("2", "Afernoon"),
-    ("3", "Evening"),
-)
-class InputForm(forms.Form):
-    first_name = forms.CharField(max_length= 200, required = False)
-    last_name = forms.CharField(max_length=200, required=False)
-    # shift = forms.ChoiceField(choices = SHIFT)
-    time_log = forms.TimeField(help_text="Enter exact time")
-    """
