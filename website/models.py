8c116d9cca7fd2647e1fdd738401eeb027917ab0
Happy Felix Chukwuma <happychukwuma@gmail>
2024-01-17 00:30:27 +0100

    ..

--- website/models.py
+++ website/models.py
@@ -14,7 +14,7 @@ class Drinks(models.Model):
 class Logger(models.Model):
     first_name = models.CharField(max_length= 200)
     last_name = models.CharField(max_length=200)
-    # shift = models.ChoiceField(choices = SHIFT)
+    # shift = models.ChoiceField(choices = SHIFT) to be refactored
     time_log = models.TimeField(help_text="Enter exact time")
 
     def __str__(self):
