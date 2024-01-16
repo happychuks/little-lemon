7a208a3d0cf18a8749d6374ebe548db7083106c6
Happy Felix Chukwuma <happychukwuma@gmail>
2024-01-16 03:56:48 +0100

    build up

---
+++ website/views.py
@@ -0,0 +1,27 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def drinks(request, drink_name):
+    drink = {
+        'mocha' : 'type of coffee',
+        'tea' : 'type of hot beverage',
+        'lemonade': 'type of refreshment'
+    }
+    choice_of_drink = drink[drink_name]
+    return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
+
+
+def home(request):
+    return HttpResponse("Welcome to Little Lemon !")
+
+def about(request):
+    return HttpResponse("About us")
+
+def menu(request):
+    return HttpResponse("Menu for Little Lemon")
+
+def book(request):
+    return HttpResponse("Make a booking")
