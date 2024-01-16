57f248280b89880e1d4d618f8886cfd501e990e8
Happy Felix Chukwuma <happychukwuma@gmail>
2024-01-16 06:36:59 +0100

    refactored admin dashboard

--- website/models.py
+++ website/models.py
@@ -21,9 +21,15 @@ class Logger(models.Model):
     # shift = models.ChoiceField(choices = SHIFT)
     time_log = models.TimeField(help_text="Enter exact time")
 
+    def __str__(self):
+        return self.first_name
+
 class Booking(models.Model):
     first_name = models.CharField(max_length = 200)
     last_name = models.CharField(max_length =200)
     guest_count = models.IntegerField()
     reservation_time = models.DateField(auto_now = True)
     comments = models.CharField(max_length = 1000)
+
+    def __str__(self):
+        return self.first_name
