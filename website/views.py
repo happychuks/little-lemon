87cfa738e80511033d3d171f482adee66b98666d
Happy Felix Chukwuma <happychukwuma@gmail>
2024-01-31 14:26:12 +0100

    updated the views

--- website/views.py
+++ website/views.py
@@ -37,8 +37,10 @@ def home(request):
     return render(request, 'home.html', {}) # follows the DRY convention
 
 def about(request):
-    return HttpResponse("About us")
+    about_content = {'about': "Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
+    return render(request, "about.html", {'content': about_content})
 
 def menu(request):
-    return HttpResponse("Menu for Little Lemon")
+    our_menu = {'menu': "We have traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12–15 items that they rotate seasonally. The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day."} 
+    return render(request, "menu.html", {'content': our_menu})
 
